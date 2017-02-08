defbd49cc3e1b122bd549395298d406bc66637c1
Ross Basarevych <basarevych@gmail.com>
2017-02-08 16:34:44 +0200

    Confirm Request

--- modules/tracker/messages/confirm-request.js
+++ modules/tracker/messages/confirm-request.js
@@ -12,10 +12,18 @@ const WError = require('verror').WError;
 class ConfirmRequest {
     /**
      * Create service
-     * @param {ErrorHelper} error   Error helper service
+     * @param {Tracker} tracker                 Tracker server
+     * @param {object} config                   Configuration
+     * @param {Logger} logger                   Logger
+     * @param {UserRepository} userRepo         User repository
+     * @param {DaemonRepository} daemonRepo     Daemon repository
      */
-    constructor(error) {
-        this._error = error;
+    constructor(tracker, config, logger, userRepo, daemonRepo) {
+        this._tracker = tracker;
+        this._config = config;
+        this._logger = logger;
+        this._userRepo = userRepo;
+        this._daemonRepo = daemonRepo;
     }
 
     /**
@@ -31,7 +39,7 @@ class ConfirmRequest {
      * @type {string[]}
      */
     static get requires() {
-        return [ 'error' ];
+        return [ 'servers.tracker', 'config', 'logger', 'repositories.user', 'repositories.daemon' ];
     }
 
     /**
@@ -40,7 +48,50 @@ class ConfirmRequest {
      * @param {object} message      The message
      */
     onMessage(id, message) {
+        let client = this._tracker.clients.get(id);
+        if (!client)
+            return;
 
+        debug(`Got CONFIRM REQUEST from ${client.socket.remoteAddress}:${client.socket.remotePort}`);
+        return this._daemonRepo.findByConfirm(message.confirmRequest.token)
+            .then(daemons => {
+                let daemon = daemons.length && daemons[0];
+                if (!daemon) {
+                    let response = this._tracker.ConfirmResponse.create({
+                        response: this._tracker.ConfirmResponse.Result.REJECTED,
+                    });
+                    let message = this._tracker.ServerMessage.create({
+                        type: this._tracker.ServerMessage.Type.CONFIRM_RESPONSE,
+                        confirmResponse: response,
+                    });
+                    let data = this._tracker.ServerMessage.encode(message).finish();
+                    return this._tracker.send(id, data);
+                }
+
+                daemon.token = this._tracker.generateToken();
+                daemon.confirm = null;
+                daemon.confirmedAt = moment();
+
+                return this._daemonRepo.save(daemon)
+                    .then(daemonId => {
+                        if (!daemonId)
+                            throw new Error('Could not save daemon');
+
+                        let response = this._tracker.ConfirmResponse.create({
+                            response: this._tracker.ConfirmResponse.Result.ACCEPTED,
+                            token: daemon.token,
+                        });
+                        let message = this._tracker.ServerMessage.create({
+                            type: this._tracker.ServerMessage.Type.CONFIRM_RESPONSE,
+                            confirmResponse: response,
+                        });
+                        let data = this._tracker.ServerMessage.encode(message).finish();
+                        this._tracker.send(id, data);
+                    });
+            })
+            .catch(error => {
+                this._logger.error(new WError(error, 'ConfirmRequest.onMessage()'));
+            });
     }
 }
 
---
+++ repositories/daemon/find-by-confirm.js
@@ -0,0 +1,61 @@
+/**
+ * DaemonRepository.findByConfirm()
+ */
+'use strict';
+
+const WError = require('verror').WError;
+
+/**
+ * Find daemons by confirmation token
+ * @method findByConfirm
+ * @memberOf module:repositories/daemon~DaemonRepository
+ * @param {string} token                    Token to search by
+ * @param {PostgresClient|string} [pg]      Will reuse the Postgres client provided, or if string then will connect to
+ *                                          this instance of Postgres.
+ * @return {Promise}                        Resolves to array of models
+ */
+module.exports = function (token, pg) {
+    return Promise.resolve()
+        .then(() => {
+            if (typeof pg == 'object')
+                return pg;
+
+            return this._postgres.connect(pg);
+        })
+        .then(client => {
+            return client.query(
+                    'SELECT * ' +
+                    '  FROM daemons ' +
+                    ' WHERE confirm = $1 ',
+                    [ token ]
+                )
+                .then(result => {
+                    return result.rowCount ? result.rows : [];
+                })
+                .then(
+                    value => {
+                        if (typeof pg != 'object')
+                            client.done();
+                        return value;
+                    },
+                    error => {
+                        if (typeof pg != 'object')
+                            client.done();
+                        throw error;
+                    }
+                );
+        })
+        .then(rows => {
+            let models = [];
+            for (let row of rows) {
+                let model = this.create();
+                this._postgres.constructor.unserializeModel(model, row);
+                models.push(model);
+            }
+
+            return models;
+        })
+        .catch(error => {
+            throw new WError(error, 'DaemonRepository.findByConfirm()');
+        });
+};
