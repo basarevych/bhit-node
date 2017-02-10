0d4b20f9b14c96acae26ab3e6fba318222f1a3a1
Ross Basarevych <basarevych@gmail.com>
2017-02-10 15:26:46 +0200

    daemon_connections methods of repos

---
+++ repositories/connection/find-by-daemon.js
@@ -0,0 +1,78 @@
+/**
+ * ConnectionRepository.findByDaemon()
+ */
+'use strict';
+
+const WError = require('verror').WError;
+
+/**
+ * Find connections by daemon
+ * @method findByDaemon
+ * @memberOf module:repositories/connection~ConnectionRepository
+ * @param {object|number} daemon            Daemon to search by
+ * @param {PostgresClient|string} [pg]      Will reuse the Postgres client provided, or if string then will connect to
+ *                                          this instance of Postgres.
+ * @return {Promise}                        Resolves to array of models
+ */
+module.exports = function (daemon, pg) {
+    let key = `sql:daemon-connections-by-daemon-id:${typeof daemon == 'object' ? daemon.id : daemon}`;
+
+    return this._cacher.get(key)
+        .then(value => {
+            if (value)
+                return value;
+
+            return Promise.resolve()
+                .then(() => {
+                    if (typeof pg == 'object')
+                        return pg;
+
+                    return this._postgres.connect(pg);
+                })
+                .then(client => {
+                    return client.query(
+                            '    SELECT c.*, dc.acting_as ' +
+                            '      FROM connections c' +
+                            'INNER JOIN daemon_connections dc' +
+                            '        ON c.id = dc.connection_id ' +
+                            '     WHERE dc.daemon_id = $1 ',
+                            [ typeof daemon == 'object' ? daemon.id : daemon ]
+                        )
+                        .then(result => {
+                            let rows = result.rowCount ? result.rows : [];
+                            if (!rows.length)
+                                return rows;
+
+                            return this._cacher.set(key, rows)
+                                .then(() => {
+                                    return rows;
+                                });
+                        })
+                        .then(
+                            value => {
+                                if (typeof pg != 'object')
+                                    client.done();
+                                return value;
+                            },
+                            error => {
+                                if (typeof pg != 'object')
+                                    client.done();
+                                throw error;
+                            }
+                        );
+                });
+        })
+        .then(rows => {
+            let models = [];
+            for (let row of rows) {
+                let model = this.create();
+                this._postgres.constructor.unserializeModel(model, row);
+                models.push(model);
+            }
+
+            return models;
+        })
+        .catch(error => {
+            throw new WError(error, 'ConnectionRepository.findByDaemon()');
+        });
+};
---
+++ repositories/daemon/find-by-connection.js
@@ -0,0 +1,78 @@
+/**
+ * DaemonRepository.findByConnection()
+ */
+'use strict';
+
+const WError = require('verror').WError;
+
+/**
+ * Find daemons by connection
+ * @method findByConnection
+ * @memberOf module:repositories/daemon~DaemonRepository
+ * @param {object|number} connection        Connection to search by
+ * @param {PostgresClient|string} [pg]      Will reuse the Postgres client provided, or if string then will connect to
+ *                                          this instance of Postgres.
+ * @return {Promise}                        Resolves to array of models
+ */
+module.exports = function (connection, pg) {
+    let key = `sql:daemon-connections-by-connection-id:${typeof connection == 'object' ? connection.id : connection}`;
+
+    return this._cacher.get(key)
+        .then(value => {
+            if (value)
+                return value;
+
+            return Promise.resolve()
+                .then(() => {
+                    if (typeof pg == 'object')
+                        return pg;
+
+                    return this._postgres.connect(pg);
+                })
+                .then(client => {
+                    return client.query(
+                            '    SELECT d.*, dc.acting_as ' +
+                            '      FROM daemons d' +
+                            'INNER JOIN daemon_connections dc' +
+                            '        ON d.id = dc.daemon_id ' +
+                            '     WHERE dc.connection_id = $1 ',
+                            [ typeof connection == 'object' ? connection.id : connection ]
+                        )
+                        .then(result => {
+                            let rows = result.rowCount ? result.rows : [];
+                            if (!rows.length)
+                                return rows;
+
+                            return this._cacher.set(key, rows)
+                                .then(() => {
+                                    return rows;
+                                });
+                        })
+                        .then(
+                            value => {
+                                if (typeof pg != 'object')
+                                    client.done();
+                                return value;
+                            },
+                            error => {
+                                if (typeof pg != 'object')
+                                    client.done();
+                                throw error;
+                            }
+                        );
+                });
+        })
+        .then(rows => {
+            let models = [];
+            for (let row of rows) {
+                let model = this.create();
+                this._postgres.constructor.unserializeModel(model, row);
+                models.push(model);
+            }
+
+            return models;
+        })
+        .catch(error => {
+            throw new WError(error, 'DaemonRepository.findByConnection()');
+        });
+};
